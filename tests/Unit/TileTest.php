2b1cf426c7d1741c9809c46c79a46eda9f2e9f88
Akos <akos@glue.be>
2024-09-27 10:36:12 +0200

    add mutation

--- tests/Unit/TileTest.php
+++ tests/Unit/TileTest.php
@@ -3,6 +3,12 @@
 use App\Tile\Color;
 use App\Tile\Tile;
 
+mutates(Color::class, Tile::class);
+
+test('total number of all colors', function () {
+    $this->assertCount(5, Color::getAll());
+});
+
 test('testISameColor_EveryColor_ColorIsRight', function () {
     foreach (Color::getAll() as $colorForTile) {
         $tile = new Tile($colorForTile);

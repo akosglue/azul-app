de175dded45d1a211e0271d765ed6e2da56817e1
Akos <akos@glue.be>
2024-09-30 16:10:04 +0200

    add mutation

--- tests/Unit/GameRoundTest.php
+++ tests/Unit/GameRoundTest.php
@@ -6,8 +6,11 @@ use App\Tile\Color;
 use App\Tile\Tile;
 use App\Tile\TileCollection;
 
+mutates(GameRound::class);
+
 test('testKeepPlaying_EmptyFactoriesAndTable_False', function () {
     $t = createGameTable();
+    $t->addToCenterPile(new TileCollection([new App\Tile\Tile(Color::YELLOW)]));
     $round = new GameRound($t,
         [
             $f = new Factory(
@@ -21,6 +24,73 @@ test('testKeepPlaying_EmptyFactoriesAndTable_False', function () {
         ]
     );
     $this->assertTrue($round->canContinue());
+    $f->take(Color::CYAN);
+    $this->assertTrue($round->canContinue());
+    $t->take(Color::YELLOW);
+    $this->assertFalse($round->canContinue());
+});
+
+test('multiple factory contents', function ($f) {
+    $t = createGameTable();
+    $round = new GameRound($t,
+        [
+            $f,
+        ]
+    );
+    $this->assertTrue($round->canContinue());
+    $f->take(Color::CYAN);
+    $this->assertFalse($round->canContinue());
+})->with([
+    new Factory(
+        new TileCollection([
+            new Tile(Color::CYAN),
+            new Tile(Color::CYAN),
+            new Tile(Color::CYAN),
+            new Tile(Color::CYAN),
+            new Tile(Color::CYAN),
+        ])
+    ),
+    new Factory(
+        new TileCollection([
+            new Tile(Color::CYAN),
+            new Tile(Color::CYAN),
+            new Tile(Color::CYAN),
+            new Tile(Color::CYAN),
+        ])
+    ),
+    new Factory(
+        new TileCollection([
+            new Tile(Color::CYAN),
+            new Tile(Color::CYAN),
+            new Tile(Color::CYAN),
+        ])
+    ),
+    new Factory(
+        new TileCollection([
+            new Tile(Color::CYAN),
+            new Tile(Color::CYAN),
+        ])
+    ),
+    new Factory(
+        new TileCollection([
+            new Tile(Color::CYAN),
+        ])
+    ),
+]);
+
+test('cannot take from empty factory', function () {
+    $this->expectException(Webmozart\Assert\InvalidArgumentException::class);
+    $t = createGameTable();
+    $round = new GameRound($t,
+        [
+            $f = new Factory(
+                new TileCollection([
+
+                ])
+            ),
+        ]
+    );
+    $this->assertFalse($round->canContinue());
     $f->take(Color::CYAN);
     $this->assertFalse($round->canContinue());
 });

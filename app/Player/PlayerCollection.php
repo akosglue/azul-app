dc5846d778330c656d37ebdd203a05c4432cf34c
Akos <akos@glue.be>
2024-10-18 14:21:36 +0200

    make stack composition io. inherit

--- app/Player/PlayerCollection.php
+++ app/Player/PlayerCollection.php
@@ -5,22 +5,37 @@ declare(strict_types=1);
 namespace App\Player;
 
 use App\Assert\Assert;
+use Traversable;
 
 /**
- * @method Player current()
+ * @implements \IteratorAggregate<Player>
  */
-
-/** @extends \SplStack<Player> */
-class PlayerCollection extends \SplStack
+class PlayerCollection implements \Countable, \IteratorAggregate
 {
+    /**
+     * @var \SplStack<Player>
+     */
+    private \SplStack $players;
+
     /**
      * @param  array<Player>  $players
      */
     public function __construct(array $players = [])
     {
+        $this->players = new \SplStack;
         Assert::allIsInstanceOf($players, Player::class);
         foreach ($players as $player) {
-            $this->push($player);
+            $this->players->push($player);
         }
+    }
+
+    public function getIterator(): Traversable
+    {
+        return $this->players;
+    }
+
+    public function count(): int
+    {
+        return $this->players->count();
     }
 }
